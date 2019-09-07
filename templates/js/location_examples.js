d98e0d59efb529376d3619672bca075d0f668182
Jonathan Northcott <northcott.j@chisigma.co>
2019-09-07 17:53:44 -0400

    unstyled static location list example

---
+++ templates/js/location_examples.js
@@ -0,0 +1,82 @@
+var locations = {
+  'zoom_level': 8,
+  'query': {
+    'name': 'Northeastern University',
+    'formatted_address': '360 Huntington Ave, Boston, MA 02115, USA',
+    'street': '360 Huntington Ave',
+    'city': 'Boston',
+    'state': 'MA',
+    'zipcode': '02115',
+    'phone': '(617) 373-2000',
+    'lat': 42.3398067,
+    'lng': -71.08917170000001,
+    'url': 'https://maps.google.com/?cid=14518198813298178444'
+  },
+  'closest_location': {
+    'name': "Cappy's Pizza",
+    'formatted_address': '82 Westland Ave, Boston, MA 02115, USA',
+    'street': '82 Westland Ave',
+    'city': 'Boston',
+    'state': 'MA',
+    'zipcode': '02115',
+    'phone': '(617) 247-8801',
+    'lat': 42.3437379,
+    'lng': -71.08965260000002,
+    'url': 'https://maps.google.com/?cid=858335234578744410',
+    'distance': 0.5
+  },
+  'locations': [
+    {
+      'name': "Cappy's Pizza",
+      'formatted_address': '82 Westland Ave, Boston, MA 02115, USA',
+      'street': '82 Westland Ave',
+      'city': 'Boston',
+      'state': 'MA',
+      'zipcode': '02115',
+      'phone': '(617) 247-8801',
+      'lat': 42.3437379,
+      'lng': -71.08965260000002,
+      'url': 'https://maps.google.com/?cid=858335234578744410',
+      'distance': 0.5
+    },
+    {
+      'name': "Chi Sigma",
+      'formatted_address': '820 Parker St, Roxbury Crossing, MA 02120, USA',
+      'street': '820 Parker St',
+      'city': 'Roxbury Crossing',
+      'state': 'MA',
+      'zipcode': '02120',
+      'phone': '(617) 420-6969',
+      'lat': 42.3278348,
+      'lng': -71.0991507,
+      'url': 'https://maps.google.com/?q=820+Parker+St,+Boston,+MA+02120,+USA&ftid=0x89e37987120c807d:0x9fa981b3a691af67',
+      'distance': 0.72
+    },
+    {
+      'name': "Portsmouth Brewery",
+      'formatted_address': '56 Market St, Portsmouth, NH 03801, USA',
+      'street': '56 Market St',
+      'city': 'Portsmouth',
+      'state': 'NH',
+      'zipcode': '03801',
+      'phone': '(603) 431-1115',
+      'lat': 43.07792449999999,
+      'lng': -70.7577647,
+      'url': 'https://maps.google.com/?cid=2896341337374084974',
+      'distance': 59.2
+    },
+    {
+      'name': "TRH-BAR",
+      'formatted_address': '3699 Boul St-Laurent, Montreal, QC H2X 2V7, Canada',
+      'street': '3699 Boul St-Laurent',
+      'city': 'Montreal',
+      'state': 'QC',
+      'zipcode': 'QC H2X 2V7',
+      'phone': '(514) 419-1416',
+      'lat': 45.51485979999999,
+      'lng': -73.57437970000001,
+      'url': 'https://maps.google.com/?cid=15514103763919835179',
+      'distance': 313
+    }
+  ]
+}
